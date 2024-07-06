3dae4bf04d45d0ab1d6e2cbae8734bd4c4351c8f
Andreea Camen <luiza.andreea90@gmail.com>
2024-07-06 20:25:01 +0300

    add: function for handling keydown event for Enter validation: name and phone should have at least minim three characters

--- app.js
+++ app.js
@@ -3,18 +3,39 @@ const phoneInputElement = document.querySelector("#phone");
 const addContactButton = document.querySelector("#add-btn");
 const table = document.querySelector("#phone-book-table");
 const phoneNumberDetails = [];
+const errorOutputParagraph = document.querySelector("#error-output");
 
 addContactButton.addEventListener("click", addNewContact);
+phoneInputElement.addEventListener("keydown", addNewContactOnPressingEnterKey);
+nameInputElement.addEventListener("keydown", addNewContactOnPressingEnterKey1);
+
+function addNewContactOnPressingEnterKey(e) {
+  if (e.key === "Enter") {
+    addNewContact();
+  }
+}
+
+function addNewContactOnPressingEnterKey1(e) {
+  if (e.key === "Enter") {
+    addNewContact();
+  }
+}
 
 function addNewContact() {
   const name = nameInputElement.value;
   const phoneNumber = phoneInputElement.value;
 
+  if (name.length < 3 || phoneNumber.length < 3) {
+    errorOutputParagraph.innerHTML =
+      "Numele si numarul de telefon trebuie sa contina minim 3 caractere";
+    errorOutputParagraph.style.color = "red";
+    return;
+  }
+
   if (phoneNumberDetails.length === 0) {
     const thead = createTableHeader();
     table.appendChild(thead);
   }
-
   phoneNumberDetails.push({
     name: name,
     phoneNumber: phoneNumber,
@@ -37,7 +58,12 @@ function addNewContact() {
   tableBody.appendChild(tableRow);
 
   table.appendChild(tableBody);
+
+  nameInputElement.value = "";
+  phoneInputElement.value = "";
+  errorOutputParagraph.innerHTML = "";
 }
+
 function createTableHeader() {
   const nameHeader = document.createElement("th");
   nameHeader.innerHTML = "Name";
